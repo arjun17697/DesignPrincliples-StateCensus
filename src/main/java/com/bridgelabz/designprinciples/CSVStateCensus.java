1fff7722ea8a69f2094790cb3ffc61b1c8831f4f
arjun17697 <arjun17697@gmail.com>
2020-10-18 23:27:59 +0530

    Refactor:Modified program to include test when header given is incorrect

--- src/main/java/com/bridgelabz/designprinciples/CSVStateCensus.java
+++ src/main/java/com/bridgelabz/designprinciples/CSVStateCensus.java
@@ -12,29 +12,37 @@ import com.opencsv.CSVReader;
 public class CSVStateCensus {
 	private static final String CSV_FILE_DELIMITER = "C:\\Users\\HP LAP\\Desktop\\BridgeLabz\\DesignPrinciples\\designprinciples\\InvalidDelimiter.CSV";
 	private static final String CSV_FILE = "C:\\Users\\HP LAP\\Desktop\\BridgeLabz\\DesignPrinciples\\designprinciples\\IndiaStateCensusData.CSV";
-
+	private static final String CSV_FILE_HEADER = "C:\\Users\\HP LAP\\Desktop\\BridgeLabz\\DesignPrinciples\\designprinciples\\InvalidHeader.CSV";;
 	public int CSVStateCensusLoader(String csvFile) throws Exception {
-		File file1= new File(CSV_FILE_DELIMITER);
-		File file2= new File(CSV_FILE);
+		File file1 = new File(CSV_FILE_DELIMITER);
+		File file2 = new File(CSV_FILE);
 		File file3 = new File(csvFile);
-		if (!(file1.compareTo(file3) == 0) && !(file2.compareTo(file3) == 0)) {
+		File file4= new File(CSV_FILE_HEADER);
+		if (!(file1.compareTo(file3) == 0) && !(file2.compareTo(file3) == 0) && !(file4.compareTo(file3) == 0)) {
 			throw new CensusException("Invalid file path", ExceptionType.INVALID_FILE_PATH);
-			}
-		
+		}
+
 		// Instantiating the CSVReader class
-		CSVReader reader = new CSVReader(new FileReader(
-				csvFile));
+		CSVReader reader = new CSVReader(new FileReader(csvFile));
+		String[] headers = reader.readNext();
+		
+		//Throw exception for invalid header
+		if (!(headers[0].equals("State") && headers[1].equals("Population") && headers[2].equals("AreaInSqKm")
+				&& headers[3].equals("DensityPerSqKm")))
+			throw new CensusException("Invalid headers", CensusException.ExceptionType.INVALID_HEADER);
 		// Reading the contents of the csv file
 		StringBuffer buffer = new StringBuffer();
 		String line[];
 		// Getting the iterator object for this reader
 		Iterator it = reader.iterator();
 		int count = 0;
+		
+		//Throw exception for invalid delimiter
 		while (it.hasNext()) {
 			line = (String[]) it.next();
-			for(String s: line){
-				if(s.contains(":"))
-        		throw new CensusException("Invalid delimiter", ExceptionType.INVALID_DELIMITER);
+			for (String s : line) {
+				if (s.contains(":"))
+					throw new CensusException("Invalid delimiter", ExceptionType.INVALID_DELIMITER);
 			}
 			System.out.println(Arrays.toString(line));
 			System.out.println(" ");
@@ -44,4 +52,3 @@ public class CSVStateCensus {
 	}
 
 }
-
--- src/test/java/com/bridgelabz/designprinciples/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/designprinciples/StateCensusAnalyserTest.java
@@ -12,6 +12,7 @@ public class StateCensusAnalyserTest {
 	private static final String CSV_FILE = "C:\\Users\\HP LAP\\Desktop\\BridgeLabz\\DesignPrinciples\\designprinciples\\IndiaStateCensusData.CSV";
 	private static final String CSV_FILE_INCOR = "C:\\Users\\HP LAP\\Desktop\\BridgeLabz\\DesignPrinciples\\designprinciples\\IndiaStateCensusData1.CSV";
 	private static final String CSV_FILE_DELIMITER = "C:\\Users\\HP LAP\\Desktop\\BridgeLabz\\DesignPrinciples\\designprinciples\\InvalidDelimiter.CSV";
+	private static final String CSV_FILE_HEADER = "C:\\Users\\HP LAP\\Desktop\\BridgeLabz\\DesignPrinciples\\designprinciples\\InvalidHeader.CSV";
 
 	/**
 	 * Rigorous Test :-)
@@ -20,14 +21,14 @@ public class StateCensusAnalyserTest {
 	@Test
 	public void givenCensusFileNumberOfRecordsMatches() throws Exception {
 		int num = new CSVStateCensus().CSVStateCensusLoader(CSV_FILE);
-		Assert.assertEquals(30, num);
+		Assert.assertEquals(29, num);
 	}
 
 	@Test
 	public void testThrowsExceptionWhenInvalidFileUsed() throws CensusException {
 		boolean thrown = false;
 		try {
-			new CSVStateCensus().CSVStateCensusLoader(CSV_FILE_INCOR);
+			new CSVStateCensus().CSVStateCensusLoader(CSV_FILE);
 		} catch (Exception e) {
 			thrown = true;
 			System.out.println("Provided file is incorrect.Please retry");
@@ -39,12 +40,25 @@ public class StateCensusAnalyserTest {
 	public void testThrowsExceptionWhenInvalidDelimiterUsed() {
 		boolean thrown = false;
 		try {
-			new CSVStateCensus().CSVStateCensusLoader(CSV_FILE_DELIMITER);
+			new CSVStateCensus().CSVStateCensusLoader(CSV_FILE);
 		} catch (Exception e) {
 			thrown = true;
 			System.out.println("Provided delimiter in file is incorrect.Please retry");
 		}
 		assertTrue(thrown);
 	}
+	
+	@Test
+	public void testThrowsExceptionWhenInvalidHeaderUsed() {
+		boolean thrown = false;
+		try {
+			new CSVStateCensus().CSVStateCensusLoader(CSV_FILE);
+		} catch (Exception e) {
+			thrown = true;
+			System.out.println("Provided header in file is incorrect.Please retry");
+		}
+		assertTrue(thrown);
+	}
+
 
 }
