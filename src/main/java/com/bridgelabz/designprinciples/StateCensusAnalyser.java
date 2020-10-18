14df44b6cc19a9bb74c5cdb70c3e307679290c58
arjun17697 <arjun17697@gmail.com>
2020-10-18 15:32:53 +0530

    Add:Welcome File

---
+++ src/main/java/com/bridgelabz/designprinciples/StateCensusAnalyser.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.designprinciples;
+
+/**
+ * Welcome Message
+ *
+ */
+public class StateCensusAnalyser 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to State Census Analyser" );
+    }
+}
